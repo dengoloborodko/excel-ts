fec49712be5431476b2a92955495c7c07102f44a
dengoloborodko <den.od.1911@gmail.com>
2020-12-13 15:18:30 +0200

    :sparkles: feat: Context property for the EventEmitter methods
    Add support of the context property for the EventEmitter listenTo and stopListening methods, which is passed as the third optional argument when you attach/detach the event.

--- src/core/EventEmitter/EventEmitter.ts
+++ src/core/EventEmitter/EventEmitter.ts
@@ -1,45 +1,65 @@
 import { EventEmitterErrors } from '../Errors';
 
+type EventEmitterListener = {
+  listener: Function;
+  context?: object;
+};
+
 type EventEmitterListeners = {
-  [key: string]: Function[];
+  [key: string]: EventEmitterListener[];
 };
 
 export class EventEmitter extends EventEmitterErrors {
   private readonly listeners: EventEmitterListeners = {};
 
-  public trigger<T extends any[]>(eventType: string, ...args: T): void {
+  public trigger<T extends (...args: any) => any>(
+    eventType: string,
+    ...args: Parameters<T>
+  ): void {
     const listeners = this.listeners[eventType];
 
     if (!listeners) {
       return console.warn(`No listeners found for event type '${eventType}'`);
     }
 
-    listeners.forEach(listener => {
+    listeners.forEach(({ listener, context }) => {
       if (listener.length !== args.length) {
         this.throwNumberOfArgumentsDoesNotMatchError(eventType);
       }
 
-      listener(...args);
+      listener.apply(context, args);
     });
   }
 
-  public listenTo<T extends Function>(eventType: string, listener: T): void {
+  public listenTo(
+    eventType: string,
+    listener: Function,
+    context?: object
+  ): void {
     if (!this.listeners[eventType]) {
       this.listeners[eventType] = [];
     }
 
-    const isCallbackExists = this.listeners[eventType].includes(listener);
-
-    if (isCallbackExists) {
-      return console.warn(
-        `This callback has already been attached for event '${eventType}'`
-      );
+    for (let listenerObject of this.listeners[eventType]) {
+      if (
+        listenerObject.listener === listener &&
+        listenerObject.context === context
+      ) {
+        return console.warn(
+          `This callback with current context has already been attached 
+        for event '${eventType}'`
+        );
+      }
     }
 
-    this.listeners[eventType].push(listener);
+    this.listeners[eventType].push({ listener, context });
   }
 
-  public stopListening(eventType: string, listenerToRemove: Function): void {
+  public stopListening(
+    eventType: string,
+    listener: Function,
+    context?: object
+  ): void {
     const listenersOfCurrentEvent = this.listeners[eventType];
 
     if (!listenersOfCurrentEvent) {
@@ -47,7 +67,16 @@ export class EventEmitter extends EventEmitterErrors {
     }
 
     this.listeners[eventType] = listenersOfCurrentEvent.filter(
-      listener => listener !== listenerToRemove
+      listenerObject => {
+        const areListenersEqual = listenerObject.listener === listener;
+        const areContextsEqual = listenerObject.context === context;
+
+        if (context) {
+          return !(areListenersEqual && areContextsEqual);
+        } else {
+          return !areListenersEqual;
+        }
+      }
     );
   }
 }
