9d0d44d0c48064593f340e87d83bd6ab283ba416
dengoloborodko <den.od.1911@gmail.com>
2020-12-13 16:51:33 +0200

    :recycle: refactor: Add protected modifier for EventEmitterErrors methods

--- src/core/Errors/EventEmitterErrors.ts
+++ src/core/Errors/EventEmitterErrors.ts
@@ -1,12 +1,12 @@
 export abstract class EventEmitterErrors {
-  throwNumberOfArgumentsDoesNotMatchError(eventType: string): never {
+  protected throwNumberOfArgumentsDoesNotMatchError(eventType: string): never {
     throw new Error(
       `The number of arguments passed to the trigger method and arguments, 
       required by a listener of the event '${eventType}', aren't equal`
     );
   }
 
-  throwNoListenersPresentForEventError(eventType: string): never {
+  protected throwNoListenersPresentForEventError(eventType: string): never {
     throw new Error(
       `No listeners present for event '${eventType}'. You need to add listeners 
         first of all`
